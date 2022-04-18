aea65f5a80596ccd0276587b175abce4491794d2
hoeding <hoeding@gmail.com>
2022-04-18 04:30:58 -0600

    Rebasing

--- src/environment_arguments.hpp
+++ src/environment_arguments.hpp
@@ -3,54 +3,160 @@
 
 #include <GNUInstallDirs.hpp>
 
-namespace options {
+namespace NOT_YET_NAMED {
+using std::is_same;
 
 class short_opt : public std::string {};
 class long_opt : public std::string {};
-class help_text : public std::string {};
-class param_value : public std::string {};
+class help_t : public std::string {};
+class param_t : public std::string {};
 using success = bool;
 using std::vector;
-enum class flags_e {
-  none = 0b0,
-  no_arg = 0b1,
-  optional_arg = 0b10,
-  mandatory_arg = 0b100,
-  error = 0b1000
+
+enum class style_e : uint8_t { 
+  none =      0b0, 
+  bool_e =    0b1, 
+  string_e =  0b10, 
+  int_e =     0b100,
+  float_e =   0b1000
 };
 
+enum class flags_e : uint8_t {
+  none =          0b0,
+  no_arg =        0b1,
+  optional_arg =  0b10,
+  mandatory_arg = 0b100,
+  error =         0b1000,
+  help =          0b10000
+};
+
+
+template <class T>
+concept pointy_boi = (std::is_pointer<T>::value);
+
+class parameter_pack{
+
+  public: /* constructors */
+  template<pointy_boi Ta>
+  parameter_pack (int const &argc_, const Ta argv_) {
+    add(argc_, argv_);
+  }
+  template<pointy_boi Ta, pointy_boi Tb>
+  parameter_pack (int const &argc_, Ta const argv_, Tb const envp_) {
+    add(argc_, argv_, envp_);
+  }
+
+  /*members*/
+  template <pointy_boi Tb, pointy_boi Tc, class... Ts>
+  void add(int const &argc_, Tb const argv_, Tc const envp_,
+           const Ts &...rest) {
+    argc = argc_;
+    argv = const_cast<char **> (argv_);
+    envp = const_cast<char **> (envp_);
+    if constexpr (sizeof...(rest) > 0) {
+      add(rest...);
+    }
+  }
+  template <int Ta, pointy_boi Tb, class... Ts>
+  void add(int const &argc_, Tb const argv_, const Ts &...rest) {
+    argc = argc_;
+    argv = argv_;
+    if constexpr (sizeof...(rest) > 0) {
+      add(rest...);
+    }
+  }
+  template <pointy_boi Ta, class... Ts>
+  void add(Ta const envp_, const Ts &...rest) {
+    envp = envp_;
+    if constexpr (sizeof...(rest) > 0) {
+      add(rest...);
+    }
+  }
+  void add(int argc_, char **argv_) { // Construct using arguments only
+    argc = argc_;
+    argv = argv_;
+  }
+  void add(char **envp_) { // Construct using environment only
+    envp = envp_;
+  };
+  void add(int argc_, char **argv_,
+           char **envp_) { // Construct using arguments and environment
+    add(argc_, argv_);
+    add(envp_);
+  };
+private:
+  int argc = 0;
+  char **argv = nullptr;
+  char **envp = nullptr;
+};
+
+template <class T>
+concept option_pack_constructable =
+    (is_same<T, short_opt>::value or is_same<T, long_opt>::value or
+     is_same<T, help_t>::value or is_same<T, param_t>::value or
+     is_same<T, style_e>::value or is_same<T, flags_e>::value
+);
 class option_pack {
 public:
-  option_pack(short_opt my_value ...){
-    short_opts.emplace_back(my_value);
-
-    };
-  void and_then([[maybe_unused]] short_opt my_value ...) {
-    short_opts.emplace_back(value);
-
+  template <option_pack_constructable T, option_pack_constructable... Ts>
+  option_pack(T const &first, Ts const &...rest) {
+    add(first);
+    if constexpr (sizeof...(rest) > 0) {
+      add(rest...);
+    }
   }
-  option_pack(long_opt ...){};
-  option_pack(help_text ...){};
-  option_pack(param_value ...){};
-  option_pack(int, char ** ...){};
-  option_pack(int, char **, char ** ...){};
 
+  template <option_pack_constructable T, option_pack_constructable... Ts>
+  void add(T const &first, Ts const &...rest) {
+    add(first);
+    if constexpr (sizeof...(rest) > 0) {
+      add(rest...);
+    }
+  }
+  bool is_it_true() {return true;};
+
+protected:
+  void add(long_opt value) { long_opts.emplace_back(value); }
+  void add(help_t value) { help_text = value; }
+  void add(param_t value) { param_value = value; }
 
 private:
- vector<short_opt> short_opts{};
-  vector<long_opt> long_opts{};
-  help_text help_text_{};
-  param_value param_value_{};
-};
-class getopts_getargs_getenv {
-  public:
-  getopts_getargs_getenv (vector<option_pack>){};
-  getopts_getargs_getenv (option_pack){};
+  vector<short_opt> short_opts;
+  vector<long_opt> long_opts;
+  help_t help_text;
+  param_t param_value;
 };
 
+template <class T>
+concept options_manager_constructable = (
+  std::is_same<T, option_pack>::value or
+  std::is_same<T, parameter_pack>::value
+);
 
+class options_manager {
+public:
+  template <options_manager_constructable Ta,
+            options_manager_constructable... Ts>
+  options_manager(Ta const &first, Ts const &...rest) {
+    add(first);
+    if constexpr (sizeof...(rest) > 0) {
+      add(rest...);
+    }
+  }
+  protected:
+  void add([[maybe_unused]] parameter_pack value) {};
+  void add([[maybe_unused]] option_pack value) { /*respect order */};
+  template<options_manager_constructable T, options_manager_constructable... Ts>
+  void add([[maybe_unused]] T const &first, Ts const &...rest){
+    add(first);
+    if constexpr (sizeof...(rest) > 0) {
+      add(rest...);
+    }
+  }
+  };
+}
 
-
+ // namespace NOT_YET_NAMED
 
 namespace environment_variables {
 
@@ -76,6 +182,7 @@ std::vector<std::filesystem::path> env_path() {
     std::string::size_type start;
     std::string::size_type end;
   };
+
   std::vector<token_t> tokens;
   std::vector<std::filesystem::path> paths{};
   std::string raw_path{getenv("PATH")};
@@ -91,8 +198,5 @@ std::vector<std::filesystem::path> env_path() {
   return paths;
 };
 } // namespace environment_variables
-namespace arguments {}
-
-} // namespace options
 
 #endif
--- src/generate_config_file.cpp
+++ src/generate_config_file.cpp
@@ -11,33 +11,91 @@ using std::filesystem::directory_iterator;
 using std::filesystem::path;
 using namespace logging;
 using namespace utility;
-using namespace options;
+using namespace NOT_YET_NAMED;
 
 /** @brief Single argument increments/decrements current brightness by N% */
-int main([[maybe_unused]] const int argc, [[maybe_unused]] const char *argv[]) {
+int main([[maybe_unused]] const int argc, [[maybe_unused]] const char *argv[], [[maybe_unused]] const char * envp[]) {
 
-  /* Configuration variables, TODO: make configurable */
+/* Configuration variables, TODO: make configurable */
+option_pack verbose {
+  short_opt{"v"}, 
+  long_opt{"verbose"}, 
+  help_t{"loquacious output, useful for debugging"}
+};
+option_pack all_users {
+  short_opt{"a"},
+  long_opt{"all_users"},
+  help_t{"Write the generated config file to default location (SHAREDSTATEDIR)"},
+  style_e::string_e
+};
+option_pack current_user {
+  short_opt{"c"},
+  long_opt{"current_user"},
+  help_t{"optional long option description"},
+  style_e::string_e
+};
+option_pack by_path {
+  short_opt{"p"},
+  long_opt{"output-path"},
+  help_t{"path to write generated configuration"},
+  style_e::string_e
+};
+option_pack help{
+  short_opt{"h"},
+  long_opt{"help"},
+  help_t{"long form help description"},
+  flags_e::error,
+  style_e::bool_e
+};
+parameter_pack arg_env { argc, argv, envp
+};
 
-  getopts_getargs_getenv opt{{
-    {short_opt{"h"}, long_opt{"help"}, help_text{"long form help description"}, flags_e::error},
-    {short_opt{"a"}, long_opt{"all_users"}, help_text{"Write the generated config file to default location (SHAREDSTATEDIR)"}},
-    {short_opt{"c"}, long_opt{"current_user"}, help_text{"optional long option description"}}
-  };
+options_manager opt{
+  arg_env, verbose, all_users, current_user, help
+};
 
-  bool all_users = true;
-  bool current_user = true;
-  
-  vector<path> valid_devices {backlight::scan_for_valid_backlights()};
-  dbg(true, 0, "Discovered devices (valid_devices) -", valid_devices);
-  if (all_users) {
-    strings_to_file_with_newline(valid_devices, "/etc/backlight/config");
+
+path manual_path = "/dev/null";
+
+/* */
+
+/* Look through the entries in /sys/class/backlight and find all supported
+  * devices */
+vector<path> valid_paths{};
+path backlight_path{"/sys/class/backlight"};
+if (exists(backlight_path) and is_directory(backlight_path)) {
+  for (path device : directory_iterator{backlight_path}) {
+    if (exists(device / "brightness")) {
+      valid_paths.emplace_back(device.filename());
+      dbg(false, 0, "Device supports brightness", device);
+    } else {
+      dbg(true, 0, "Device does not support brightness", device);
+    }
+  }
+} else {
+  dbg(true, 0,
+      "/sys/class/backlight/ path does not exist. Please ensure that \
+      the necessary kernel options are enabled (kernel option: BACKLIGHT_CLASS_DEVICE)");
+  std::exit(EXIT_FAILURE);
+}
+
+  /* */
+
+#ifndef NDEBUG
+  dbg(true, 0, "Debug - valid_paths", valid_paths);
+  dbg(true, 0);
+#endif
+  if (all_users.is_it_true()) {
     // install to /etc/backlight/config
   };
 
-  if (current_user) {
-    strings_to_file_with_newline(valid_devices, backlight::get_xdg_config_path());
+  if (current_user.is_it_true()) {
     // install to XDG config dir "~/.config/backlight/config"
   };
+
+  if (by_path.is_it_true()) {
+    // install to given file
+  }
 
   return (EXIT_SUCCESS);
 }
