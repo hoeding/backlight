eccb7caa118379d52ad7b5df47fb217e16579dc1
hoeding <hoeding@gmail.com>
2022-05-05 02:10:23 -0600

    dead code

--- src/generate_config_file.cpp
+++ src/generate_config_file.cpp
@@ -20,34 +20,7 @@ int main([[maybe_unused]] const int argc, [[maybe_unused]] const char *argv[]) {
   bool current_user = true;
   path manual_path = "/dev/null";
 
-  /* */
 
-  /* Look through the entries in /sys/class/backlight and find all supported
-   * devices */
-  vector<path> valid_paths{};
-  path backlight_path{"/sys/class/backlight"};
-  if (exists(backlight_path) and is_directory(backlight_path)) {
-    for (path device : directory_iterator{backlight_path}) {
-      if (exists(device / "brightness")) {
-        valid_paths.emplace_back(device.filename());
-        dbg(false, 0, "Device supports brightness", device);
-      } else {
-        dbg(true, 0, "Device does not support brightness", device);
-      }
-    }
-  } else {
-    dbg(true, 0,
-        "/sys/class/backlight/ path does not exist. Please ensure that \
-        the necessary kernel options are enabled (kernel option: BACKLIGHT_CLASS_DEVICE)");
-    std::exit(EXIT_FAILURE);
-  }
-
-  /* */
-
-#ifndef NDEBUG
-  dbg(true, 0, "Debug - valid_paths", valid_paths);
-  dbg(true, 0);
-#endif
   vector<path> valid_devices {backlight::scan_for_valid_backlights()};
   dbg(true, 0, "Discovered devices (valid_devices) -", valid_devices);
   if (all_users) {
