07a802bf25aaf98cb139f8022983e65bd8d1ff2c
hoeding <hoeding@gmail.com>
2024-01-02 12:01:30 -0600

    merge (#4)
    * Fix NaN as brightness when device not found.
    
    * filesystem includes, env-arg-parser cmake
    
    * settings.json

---
+++ src/filesystem.hpp
@@ -0,0 +1,15 @@
+#ifndef FILESYSTEM_HPP
+#define FILESYSTEM_HPP
+
+/** @brief Macro to include the host c++ filesystem implementation */
+#if __has_include(<filesystem>)
+  #include <filesystem>
+  namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+  #include <experimental/filesystem>
+  namespace fs = std::experimental::filesystem;
+#else
+  error "Missing the <filesystem> header."
+#endif
+
+#endif
